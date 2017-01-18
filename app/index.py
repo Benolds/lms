f8ce84e0599d9c8b54297fee6244bf58280c4aae
Benjamin Reynolds <reynoldsben94@gmail.com>
2017-01-17 22:06:14 -0500

    skeleton routes for module composer and viewer

--- app/index.py
+++ app/index.py
@@ -1,9 +1,33 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/new-module', methods=['GET','POST'])
+def new_module():
+  if request.method == 'GET':
+    return show_module_composer()
+  elif request.method == 'POST':
+    return create_new_module(request.data)
+
+@app.route('/module/<id>', methods=['GET'])
+def view_module(id):
+  if request.method == 'GET':
+    return show_module_viewer(id)
+
+def show_module_composer():
+  return render_template('new-module.html', name='Ben')
+
+def create_new_module():
+  return 'create new module'
+
+def show_module_viewer(id):
+  return 'id ' + id 
 
 if (__name__ == '__main__'):
 	app.run(debug=True, threaded=True)
